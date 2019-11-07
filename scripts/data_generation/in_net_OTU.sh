4fe677b68a71e35e9bee4a61f327030f10e5b2d5
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2019-11-07 14:56:50 +0000

    Changing r version

--- scripts/data_generation/in_net_OTU.sh
+++ scripts/data_generation/in_net_OTU.sh
@@ -4,14 +4,13 @@
 #$ -j y     # Maximum output, inc errors
 #$ -r y     # Condense error files
 #$ -pe smp 1     # Request CPU cores
-#$ -l h_rt=150:00:00     # Request runtime (up to 240 hours)
-#$ -l h_vmem=50G     # Request RAM per core
-#$ -m bea     # Status emails
-#$ -t 1-47
+#$ -l h_rt=240:00:00     # Request runtime (up to 240 hours)
+#$ -l h_vmem=15G     # Request RAM per core
+#$ -t 1-48
 
 
 
-module load R
+module load R/3.4.3
 
 
 Rscript /data/scratch/btw863/network_OTUs_paper/scripts/data_generation/fine_network_netlevel.R ${SGE_TASK_ID}
